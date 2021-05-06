6307bdd7c2124e49c24a3c1af387b832559efb10
AhhhKz <arkkunatgan@gmail.com>
2021-05-06 16:35:21 +0700

    Assign_Exercise4

---
+++ Exercise4_Kunatgan_Rat.py
@@ -0,0 +1,9 @@
+scoreEng = 50.5
+scoreBusiness =  65.36
+scoreCom = 75.52
+scoreProgram = 80.5
+print("--- Your Score ---")
+print("Foundation English :",scoreEng)
+print("General Business :",scoreBusiness)
+print("Introduction to Computer Systems :",scoreCom)
+print("Computer Programming :",scoreProgram)
