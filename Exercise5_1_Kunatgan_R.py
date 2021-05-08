9dc5de4161f29c62ae6bb6a60737d36bb141f737
AhhhKz <arkkunatgan@gmail.com>
2021-05-08 09:55:04 +0700

    Create Exercise5_1

---
+++ Exercise5_1_Kunatgan_R.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter Num1 :"))
+num2 = int(input("Enter num2 :"))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = int(num1 * num2)
+result4 = int(num1 / num2)
+
+print(num1,"+",num2,"=",result1)
+print(num1,"-",num2,"=",result2)
+print(num1,"*",num2,"=",result3)
+print(num1,"/",num2,"=",result4)
